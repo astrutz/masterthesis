672ec9229882b43091b14959f212233668f814d6
Alexander Strutz <astrutz@th-koeln.de>
2022-09-23 18:31:31 +0200

    Implement user database validations

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -2,4 +2,8 @@
 
 class Recipient < ApplicationRecord
   belongs_to :inbox
+  has_secure_password
+
+  validates :email_address, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end

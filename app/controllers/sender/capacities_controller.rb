2922a985f8e97ce99905f9b3f0e24210f166f90e
Alexander Strutz <astrutz@th-koeln.de>
2022-09-24 11:25:47 +0200

    Show mails capacities from database directly

--- app/controllers/sender/capacities_controller.rb
+++ app/controllers/sender/capacities_controller.rb
@@ -3,9 +3,12 @@
 module Sender
   class CapacitiesController < ApplicationController
     def index
-      @recipient = params[:recipient]
-      @mails = params[:mails] ? params[:mails].to_i : 107
-      @performance = params[:performance] ? params[:performance].to_i : 9
+      @recipient_address = params[:recipient]
+      recipient = Recipient.find_by(email_address: @recipient_address)
+      inbox = Inbox.find_by(recipient: recipient)
+      @mails_count = Message.where(inbox: inbox).length
+      @editing_performance = recipient.editing_performance_per_day
+      @target_date = (@mails_count / @editing_performance + 1).to_f.ceil.days.from_now.to_date
     end
   end
 end
--- test/controllers/sender/capacities_controller_test.rb
+++ test/controllers/sender/capacities_controller_test.rb
@@ -5,24 +5,23 @@ require 'test_helper'
 module Sender
   class CapacitiesControllerTest < ActionDispatch::IntegrationTest
     test '/send/capacity should respond with 200' do
-      get '/send/capacity'
+      recipient_address = recipients(:one).email_address
+      get "/send/capacity?recipient=#{recipient_address}"
       assert_response :success
     end
 
     test 'should only show alert with more than 50 emails' do
-      get '/send/capacity?mails=200'
-      assert_response :success
-      assert_select '.alert'
-
-      get '/send/capacity?mails=10'
+      recipient = recipients(:one)
+      get "/send/capacity?recipient=#{recipient.email_address}"
       assert_response :success
       assert_select '.alert', 0
     end
 
     test 'should show correct response date' do
-      get '/send/capacity?mails=40&performance=7'
+      recipient = recipients(:one)
+      get "/send/capacity?recipient=#{recipient.email_address}"
       assert_response :success
-      assert_match I18n.l(6.days.from_now.to_date), response.body
+      assert_match I18n.l(1.days.from_now.to_date), response.body
     end
   end
 end

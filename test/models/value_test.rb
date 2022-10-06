0f98517606cd6e08559731128c8e774b5451bc00
Alexander Strutz <astrutz@th-koeln.de>
2022-10-06 18:26:54 +0200

    Remove send_at from Value to use created_at

--- test/models/value_test.rb
+++ test/models/value_test.rb
@@ -4,13 +4,13 @@ require 'test_helper'
 
 class ValueTest < ActiveSupport::TestCase
   test 'should create value' do
-    value = Value.new(sender_address: '', recipient_address: '', send_at: Time.now, amount: 1)
+    value = Value.new(sender_address: '', recipient_address: '', amount: 1)
     value.save
     assert true
   end
 
   test 'should have uuid' do
-    value = Value.new(sender_address: '', recipient_address: '', send_at: Time.now, amount: 1)
+    value = Value.new(sender_address: '', recipient_address: '', amount: 1)
     value.save
     assert_not_nil value.uuid
   end

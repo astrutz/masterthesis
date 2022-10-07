de96aff5581f1d240c66714083b398d500f112f6
Alexander Strutz <astrutz@th-koeln.de>
2022-10-07 17:08:31 +0200

    Fix missing credentials for credential test

--- test/models/credential_test.rb
+++ test/models/credential_test.rb
@@ -3,10 +3,9 @@
 require 'test_helper'
 
 class CredentialTest < ActiveSupport::TestCase
-  # FIXME: ActiveRecord::Encryption::Errors::Configuration: key_derivation_salt is not configured.
-  # test 'should create Credential' do
-  #   credential = Credential.new(server: '', port: '', ssl: '', auth_type: '', username: '', password: 'pass', recipient: recipients(:one))
-  #   credential.save
-  #   assert true
-  # end
+  test 'should create Credential' do
+    credential = Credential.new(server: '', port: '', ssl: '', auth_type: '', username: '', password: 'pass', recipient: recipients(:one))
+    credential.save
+    assert true
+  end
 end

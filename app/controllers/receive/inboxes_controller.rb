588876dd06be1d29d0af981cb0f8f67f4448dde5
astrutz <alex-strutz@t-online.de>
2022-10-24 15:10:50 +0200

    Fix calculation of due dates in recipient table

--- app/controllers/receive/inboxes_controller.rb
+++ app/controllers/receive/inboxes_controller.rb
@@ -15,7 +15,7 @@ module Receive
       due_dates = []
       due_date = DateTime.current.to_date
       @inbox.messages.unprocessed.with_value(sorted: true).each_with_index do |_message, index|
-        due_date += 1.day if ((index + @messages_due_today) % @inbox.recipient.editing_performance_per_day).zero?
+        due_date += 1.day if ((index + @messages_due_today) % @inbox.recipient.editing_performance_per_day).zero? && index >= @messages_due_today
         due_dates << due_date
       end
       due_dates
